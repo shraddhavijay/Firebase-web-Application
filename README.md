2c76e9ee866e282ce72de9b474ccf379d62f23d1
Manu1ND <manjunath2000@hotmail.com>
2021-05-06 20:33:31 +0530

    commented out config file, added readme

---
+++ README.md
@@ -0,0 +1,24 @@
+# Firebase-web-Application Session PPT
+
+`https://docs.google.com/presentation/d/1kKHeLlanAfiw1mmTtZGV38tX_zlG4WT5ZwKxFygOrD4/edit?usp=sharing`
+
+# Firebase-web-Application
+
+Generate and add your own configuration inside config.js
+
+# Offical Firebase Docs
+
+https://firebase.google.com/docs/database
+
+# Firebase Funtions Used
+
+1. Insert
+2. Fetch (Event Listeners)
+3. Update
+4. Delete
+
+Firebase funtions used are stored in Functions Implementations.
+
+# Sample Web Application
+
+A simple HTML-JS based web application which uses Firebase Realtime Databse to add fetch data.
--- config.js
+++ config.js
@@ -1,16 +1,17 @@
 // TODO: Replace the following with your app's Firebase project configuration
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-	apiKey: "AIzaSyDHPa5yomuJ9MfrIljX1Hmw0X2K-_0dtQc",
-	authDomain: "sakec-nosql-46351.firebaseapp.com",
-	databaseURL: "https://sakec-nosql-46351-default-rtdb.asia-southeast1.firebasedatabase.app",
-	projectId: "sakec-nosql-46351",
-	storageBucket: "sakec-nosql-46351.appspot.com",
-	messagingSenderId: "689703641796",
-	appId: "1:689703641796:web:688962492f2cc8e6999f91",
-	measurementId: "G-PXCRKGVT65"
-};
+
+// ------Replace config data------
+/* var firebaseConfig = {
+	apiKey: "AIzaSyD4M55jU5yi5PVGa6SvZ2DzqgfMfxq6Yk0",
+	authDomain: "sakec-nosql-3c1b5.firebaseapp.com",
+	databaseURL: "https://sakec-nosql-3c1b5-default-rtdb.asia-southeast1.firebasedatabase.app",
+	projectId: "sakec-nosql-3c1b5",
+	storageBucket: "sakec-nosql-3c1b5.appspot.com",
+	messagingSenderId: "1037962972462",
+	appId: "1:1037962972462:web:de783c7bcd9757e1f3bebf"
+}; */
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
